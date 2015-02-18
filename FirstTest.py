5dbc98defd8a7db7eddfca7cd298a557aebecd29
Adam Manuel <adam.m.manuel@gmail.com>
2015-02-17 18:22:44 -0600

    I don't remember
    Did this a long time ago...

--- FirstTest.py
+++ FirstTest.py
@@ -9,16 +9,16 @@ toshow = image1.load()
 toshow2 = image3.load()
 sizex = image1.size[0]
 sizey = image1.size[1]
-#image1.show()
-#image2.show()
+image1.show()
+image2.show()
 #IDK honestly
-#for x in range(sizex):
-#    for y in range(sizey):
-#        if((toshow[x,y][0]+toshow[x,y][1]+toshow[x,y][2])/3 > 200):
-#            toshow[x,y] = toshow[sizex-(x+1), y]
-#image1.show()
+for x in range(sizex):
+    for y in range(sizey):
+        if((toshow[x,y][0]+toshow[x,y][1]+toshow[x,y][2])/3 > 200):
+            toshow[x,y] = toshow[sizex-(x+1), y]
+image1.show()
 #wut
-
+''''
 temp = [None]*600
 for x in range(sizex):
     for y in range(sizey):
@@ -33,3 +33,4 @@ for x in range(sizex):
     for y in range(sizey):
         toshow2[x,y] = temp[y]
 image3.show()
+'''''
